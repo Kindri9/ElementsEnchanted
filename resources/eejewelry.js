4e42570806937a2926b027abbd74303f86b43542
Michelle Simmons <michelle.simmons@capitalone.com>
2018-04-19 10:51:03 -0700

    Learning Pink Boxes

--- resources/eejewelry.js
+++ resources/eejewelry.js
@@ -1,4 +1,65 @@
-console.log ('Hello Console');
-for(var i = 0; i < 11; i += 1) {
-    console.log(i);
+// FOR LOOPS, CONSOLE LOGGING, REVERSING AND PRINTING ONE CHARACTER AT A TIME ON A LINE
+
+var str = 'Boo';
+console.log("My string has a length of: " + str.length);
+for (var i = str.length-1; i >= 0; i--) {
+  console.log("index: " + i + " has a character of: " + str.charAt(i));
+
 }
+
+//BASIC FUNCTION - THAT WORKS
+
+console.log(myFunction('I think I can, I think I can.'));
+
+function myFunction(engine) {
+ return engine + " = " + engine.length;
+
+}
+
+
+//BASIC FUNCTION WITH PARAMETERS
+
+var me = "Hi Michelle";
+var you = "Hi Mark";
+
+console.log(weWorkTogether(you, me));
+
+function weWorkTogether(me,you) {
+  return me + " " + you;
+
+}
+
+
+// ATTEMPT AT REVERSING STRING VIA FUNCTION
+var crazy = 'Going Crazy';
+var newstuff = reverseIt(crazy);
+console.log(newstuff);
+
+function reverseIt(leSigh) {
+  var gettingIt = '';
+  for (var i = leSigh.length - 1; i >= 0; i--) {
+    gettingIt = gettingIt + leSigh.charAt(i);
+  }
+  return gettingIt;
+
+}
+extra = 'Tony';
+extra = reverseIt(extra);
+console.log(extra)
+
+
+//  SHOWING THE MIND FUCK OF PASSING THROUGH FUNCTIONS
+
+// var a = 'b';
+// var b = 'c';
+// var c = 'd';
+// var d = 'e';
+//
+// var result = backwardsOrForwards();
+//
+// console.log(result);
+//
+// function backwardsOrForwards(a, b, ){
+//   var d = 'a';
+//   return b + a + d + c;
+// }
